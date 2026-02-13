94e4b650abc9b0ade0931f5b41186a6023e94e59
Alesya Afanasenkova <147429589+Alisbliss@users.noreply.github.com>
2026-02-13 20:42:00 +0300

    Changed main screen

--- Interesnoitochka/AppDelegate.swift
+++ Interesnoitochka/AppDelegate.swift
@@ -14,7 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         if let sessionId = sessionManager.currentSessionID {
-                  print("✅ Сессия уже существует: \(sessionId)")
               } else {
                   Task {
                       await sessionManager.fetchNewSession()
