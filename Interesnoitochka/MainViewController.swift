6e700b0f0aa9df1d27522f50324194d838402d8c
Alesya Afanasenkova <147429589+Alisbliss@users.noreply.github.com>
2026-02-13 20:42:00 +0300

    Changed main screen

--- Interesnoitochka/MainViewController.swift
+++ Interesnoitochka/MainViewController.swift
@@ -5,11 +5,29 @@
 //  Created by Алеся Афанасенкова on 13.02.2026.
 //
 import UIKit
+import SnapKit
 
 class MainViewController: UIViewController {
+    
+    private let welcomeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "HOME"
+        label.font = .systemFont(ofSize: 64, weight: .black)
+        label.textColor = .white
+        label.textAlignment = .center
+        return label
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(hex: "#25282D")
+        setupLayout()
+    }
+    
+    private func setupLayout() {
+        view.addSubview(welcomeLabel)
+        welcomeLabel.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
     }
 }
